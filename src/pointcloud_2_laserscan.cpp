56b5bff5403fd39e2c43d99643b59ac73620e630
bigbigpark <scsc1125@gmail.com>
2022-08-08 19:11:40 +0900

    convert

--- src/pointcloud_2_laserscan.cpp
+++ src/pointcloud_2_laserscan.cpp
@@ -12,9 +12,11 @@
 #include <ros/ros.h>
 #include <iostream>
 #include <string>
+#include <cmath>
 
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
 
 struct LaserScanParameter
 {
@@ -26,9 +28,11 @@ struct LaserScanParameter
   double angle_min = -3.14; // -3.14
   double angle_max =  3.14; // 3.14
   double angle_increment =  0.00174532889; //[rad]
-  double scan_time = 1.0;
+  double scan_time = 1.0; // time between scans [seconds]
   double range_min = 0.01;
   double range_max = 100.0;
+
+  double inf_epsilon = 1.0;
   bool use_inf = true;
 
   /*
@@ -66,8 +70,48 @@ public:
       ros::spinOnce();
 
       // Convert PointCloud to LaserScan
+      laserscan_msgs.header = pointcloud_msgs.header;
 
+      laserscan_msgs.angle_min = param_.angle_min;
+      laserscan_msgs.angle_max = param_.angle_max;
+      laserscan_msgs.angle_increment = param_.angle_increment;
+      laserscan_msgs.time_increment = 0.0;
+      laserscan_msgs.scan_time = param_.scan_time;
+      laserscan_msgs.range_min = param_.range_min;
+      laserscan_msgs.range_max = param_.range_max;
+      
+      // Determine amount of rays to create 
+      uint32_t ranges_size = std::ceil( (laserscan_msgs.angle_max - laserscan_msgs.angle_min) / laserscan_msgs.angle_increment);
 
+      // Determine if laserscan rays with no obstacle data will evaluate to INF or max_range
+      if (param_.use_inf)
+      {
+        laserscan_msgs.ranges.assign(ranges_size, std::numeric_limits<double>::infinity());
+      }
+      else
+      {
+        laserscan_msgs.ranges.assign(ranges_size, laserscan_msgs.range_max + param_.inf_epsilon);
+      }
+
+      // Iterate through pointcloud
+      for (sensor_msgs::PointCloud2ConstIterator<float> iter_x(pointcloud_msgs, "x"), iter_y(pointcloud_msgs, "y"), iter_z(pointcloud_msgs, "z")
+          ; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z)
+      {
+        // Check NaN value
+        if (std::isnan(*iter_x) || std::isnan(*iter_y) || std::isnan(*iter_z))
+        {
+          ROS_WARN("Rejected for NaN in points(%f, %f, %f)", *iter_x, *iter_y, *iter_z);
+          continue;
+        }
+
+        // Check out of ROI using max_height && min_height
+        if (*iter_z > param_.max_height || *iter_z < param_.min_height)
+        {
+          ROS_WARN("Rejected for out of ROI (%f, %f, %f)", *iter_x, *iter_y, *iter_z);
+          continue;
+        }
+
+      }
 
       // Publish LaserScan
       laser_pub_.publish(laserscan_msgs);
